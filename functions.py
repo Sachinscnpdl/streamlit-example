814f677c053f9b0044ed24a403da96df0a66d318
Sachin Poudel <98536450+Sachinscnpdl@users.noreply.github.com>
2023-05-14 23:38:42 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -7,7 +7,16 @@ import streamlit as st
 import pymatgen
 import matminer
 from pymatgen.core.composition import Composition
-from pymatgen import Element
+#Import Libraries
+from pymatgen.core.composition import Composition, Element
+from pymatgen.core.structure import SiteCollection
+from matminer.featurizers.composition.alloy import Miedema, WenAlloys,YangSolidSolution
+from matminer.featurizers.composition import ElementFraction
+from matminer.featurizers.conversions import StrToComposition
+from matminer.utils.data import MixingEnthalpy, DemlData
+from matminer.utils import data_files #for importing "Miedema.csv" present inside package of Matminer library
+from matplotlib.ticker import MultipleLocator # for minor tick lines
+import seaborn as sns
 
 def properties_calculation(dataframe):
     
